316b4fbaa4b0b0b57ffeec9cc74a68c708ca89ef
sctnightcore <23263315+sctnightcore@users.noreply.github.com>
2022-03-08 16:54:58 +0700

    fix bug

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ class Main(Log):
         options.add_argument("--log-level=3")
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
-        options.addArguments("--headless");
-        options.addArguments("--disable-gpu");
-        
+        options.add_argument("--headless");
+        options.add_argument("--disable-gpu");
+
         user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
         options.add_argument(f'user-agent={user_agent}')
 
