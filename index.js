359692289cdcaf0827bc5a59ef89df365e53e55f
sctnightcore <23263315+sctnightcore@users.noreply.github.com>
2022-08-20 17:35:47 +0700

    add new / run ci

--- index.js
+++ index.js
@@ -11,11 +11,7 @@ const read_json = async () => {
 };
 
 const write_json = async (data) => {
-  return fs.writeFile(
-    "./data/member.json",
-    JSON.stringify(data, null, 2),
-    "utf-8"
-  );
+  return fs.writeFile("./data/member.json", JSON.stringify(data, null, 2), "utf-8");
 };
 
 const get_like = async (page, page_id) => {
@@ -41,12 +37,12 @@ const main = async () => {
   for (let i of member_data["data"]) {
     await page.goto(i["url"], { waitUntil: "networkidle2" });
 
-    let raw_message = await page.evaluate(() =>  document.documentElement.outerHTML);
+    let raw_message = await page.evaluate(() => document.documentElement.outerHTML);
 
-    let like = Number(raw_message.match(/"cannot_see_top_custom_reactions":{"reactors":{"count":(\d+)}/i)[1])
-    let share = Number(raw_message.match(/"share_count":{"count":(\d+),"is_empty":false}/i)[1])
+    let like = Number(raw_message.match(/"cannot_see_top_custom_reactions":{"reactors":{"count":(\d+)}/i)[1]);
+    let share = Number(raw_message.match(/"share_count":{"count":(\d+),"is_empty":false}/i)[1]);
 
-    let point = share * 5 + like;
+    let point = share * 3 + like;
 
     i["data"]["old_like"] = i["data"]["like"];
     i["data"]["old_share"] = i["data"]["share"];
@@ -56,18 +52,11 @@ const main = async () => {
     i["data"]["share"] = share;
     i["data"]["point"] = point;
 
-
-    console.log(
-      `[${i["type"]}${i["id"]}] Point: ${point} Like: ${like} Share: ${share}`
-    );
-
+    console.log(`[${i["type"]}${i["id"]}] Point: ${point} Like: ${like} Share: ${share}`);
   }
 
   member_data["updated_at"] = String(
-    new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60 * 1000)
-      .toJSON()
-      .slice(0, 19)
-      .replace("T", " ")
+    new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60 * 1000).toJSON().slice(0, 19).replace("T", " ")
   );
 
   await write_json(member_data);
