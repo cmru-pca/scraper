9279a6de3987ed656783057c6040135dbc34d481
sctnightcore <23263315+sctnightcore@users.noreply.github.com>
2023-08-28 23:16:25 +0700

    init 2023

--- index.js
+++ index.js
@@ -14,20 +14,6 @@ const write_json = async (data) => {
   return fs.writeFile("./data/member.json", JSON.stringify(data, null, 2), "utf-8");
 };
 
-const get_like = async (page, page_id) => {
-  const code = `//*[@id="like_sentence_${page_id}"]`;
-  const element = await page.$x(code);
-  const raw_message = await page.evaluate((el) => el.textContent, element[0]);
-  return Number(raw_message.match(/.(\d+).others/i)[1]);
-};
-
-const get_share = async (page, page_id) => {
-  const code = `//*[@id="ufi_${page_id}"]/div/div[3]/a/span`;
-  const element = await page.$x(code);
-  const raw_message = await page.evaluate((el) => el.textContent, element[0]);
-  return Number(raw_message.replace(",", "").match(/(\d+).shares/i)[1]);
-};
-
 const main = async () => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
@@ -42,7 +28,7 @@ const main = async () => {
     let like = Number(raw_message.match(/"cannot_see_top_custom_reactions":{"reactors":{"count":(\d+)}/i)[1]);
     let share = Number(raw_message.match(/"share_count":{"count":(\d+),"is_empty":false}/i)[1]);
 
-    let point = share * 3 + like;
+    let point = share * 5 + like;
 
     i["data"]["old_like"] = i["data"]["like"];
     i["data"]["old_share"] = i["data"]["share"];
